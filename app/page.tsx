2b1942fb43bf5265814a5dbef8381ed84f733012
xaviercaldwell <93307817+xaviercaldwell@users.noreply.github.com>
2026-07-30 15:28:46 -0600

    Tiny changes
    done at work to try to implement an error page, looks like you have to go about it a very specific way. look it up.

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-centerfont-sans ">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-        
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-5xl font-bold leading-10 tracking-tight ">
             AssetTrac
           </h1>
           <p className="max-w-md text-lg leading-8 ">
             Looking to do more than browse?{" "}
-            <a
-              href="/error"
-              className="font-medium "
-            >
+            <Link href="/error" className="font-medium">
+           
               Log in
-            </a>
+
+            </Link>
             .
           </p>
         </div>
@@ -28,7 +26,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert invert"
+              className=" invert"
               src="/vercel.svg"
               alt="Vercel logomark"
               width={16}
@@ -37,7 +35,7 @@ export default function Home() {
             Deploy Now
           </a>
           <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent bg-foreground text-background hover:bg-[#ccc] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent bg-foreground text-background hover:bg-[#ccc]  md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
