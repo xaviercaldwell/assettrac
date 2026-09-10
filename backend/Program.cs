84492245c692ce972d2fdb372c04da3aaa882a44
xaviercaldwell <xvrianreed@gmail.com>
2026-09-09 23:28:05 -0600

    update program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using assettrac.api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AssetTracDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AssetTracDb")));
+
+
+
 
 var app = builder.Build();
 
