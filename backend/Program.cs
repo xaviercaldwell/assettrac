beb4a102b07d603dd6c2c5d240defcc5ae6e745d
xaviercaldwell <93307817+xaviercaldwell@users.noreply.github.com>
2026-09-10 15:04:35 -0600

    Clean progarmcs from yesterday.
    Removed the boilerplate code from programcs, added AddControllers for controller classes, the mapping of said controllers, added open api, mapped open api, along with run. Had to read docs for each to understand their functionality. left comments for what goals the methods help achieve

--- backend/Program.cs
+++ backend/Program.cs
@@ -5,9 +5,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+//enable services for openapi and controller support a way to handle dbcontext
 builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<AssetTracDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AssetTracDb")));
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AssetTracDbContext>(options =>
+ options.UseSqlServer(
+    builder.Configuration.GetConnectionString("AssetTracDb")));
 
 
 
@@ -17,33 +23,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    //actually map the added service for open api to all endpoints
     app.MapOpenApi();
 }
-
+//redirets http to https
 app.UseHttpsRedirection();
+ //actually map the added service for controllers to all endpoints 
+app.MapControllers();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+//start app and listen for http reqs
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
